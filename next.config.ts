56a48d0df017089c2b06f71e5321054ce520a846
Krishnarajbhar767 <bloggerkrishna767@gmail.com>
2025-09-10 17:53:22 +0530

    Config file changed

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    output: "export", // enables static export
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
 
 export default nextConfig;
